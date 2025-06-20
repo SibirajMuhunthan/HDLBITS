8c0d81ac32a444b8521885cb0ce7d6ac8c8bbd2f
SIBIRAJ MUHUNTHAN <161289055+SibirajMuhunthan@users.noreply.github.com>
2025-06-20 19:26:01 +0530

    Create VECTORGATES.v

---
+++ VERILOG LANGUAGE/VECTORS/VECTORGATES.v
@@ -0,0 +1,10 @@
+module top_module( 
+    input [2:0] a,
+    input [2:0] b,
+    output [2:0] out_or_bitwise,
+    output out_or_logical,
+    output [5:0] out_not
+);
+    wire w1,w2;
+    assign out_or_bitwise=a|b,out_or_logical=a||b,out_not={~b,~a};
+endmodule
